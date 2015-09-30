42bea26fa3e7c31ffbc5c00f713d64bef8b66350
Adam Lay <adam93.lay+git@gmail.com>
2015-09-30 22:31:11 +0100

    Updating theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  root 'home#index'
+
+  get '/home' => 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
