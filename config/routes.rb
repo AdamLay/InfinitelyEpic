6e4652e700c55e576c3f3bfcaad9bd5383a25fd5
Adam Lay <adam93.lay+git@gmail.com>
2015-10-21 20:25:15 +0100

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   resources 'posts'
 
+  get '/login' => 'authentication#new'
+  post '/login' => 'authentication#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
