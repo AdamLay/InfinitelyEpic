1498c27216b002a56efc7e7d599836a591126219
Adam Lay <adam93.lay+git@gmail.com>
2015-10-26 18:54:22 +0000

    Contact

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-%w( posts home authentication users ).each do |controller|
+%w( posts home authentication users contact ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css"]
 end
